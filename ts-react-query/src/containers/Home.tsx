a29c666306e0cc895ae6be8afe17bedfd9b8d5cf
yme1313 <83192170+yme1313@users.noreply.github.com>
2023-04-25 17:06:08 +0900

    20230425 backup

--- ts-react-query/src/containers/Home.tsx
+++ ts-react-query/src/containers/Home.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+  const [text, setText] = useState<string>('')
 
   return (
     <Wrapper>
